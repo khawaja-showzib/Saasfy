66861a5f48806d8ff030bd6814411ac9a292e1e4
khawaja-showzib <khawajashowzib22@gmail.com>
2022-12-06 12:39:21 +0500

    finished builed

--- components/Header.js
+++ components/Header.js
@@ -50,7 +50,7 @@ const Header = () => {
                 </section>
 
                 <div ref={ref} className='h-[100vh] overflow-y-auto absolute top-0 right-0 bg-blue-100 p-10 translate-x-full transition-transform transform w-80 z-20'>
-                <span className='font-extrabold text-2xl text-primary-400'>SaasyFy</span>
+                <Link href='/'><span className='font-extrabold text-2xl text-primary-400'>SaasyFy</span></Link>
                 <RxCross1 className='text-2xl absolute top-10 right-10' onClick={toggleMenu} />
                 <ul className='flex flex-col gap-4 mt-10'>
                     <Link href='#'><li className='text-lg font-medium border-b-2 border-transparent transition duration-500 hover:border-primary-400'>About</li></Link>
